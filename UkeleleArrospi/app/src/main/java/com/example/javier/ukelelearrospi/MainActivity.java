0640579a4156eefbdf3f9a054d47e11867db110b
aorbegozo005 <aorbegozo005@ikasle.ehu.es>
2015-12-12 00:16:07 +0100

    Navigation drawer botoiak funtzionatzen jarri activitiekin aunkesea

--- UkeleleArrospi/app/src/main/java/com/example/javier/ukelelearrospi/MainActivity.java
+++ UkeleleArrospi/app/src/main/java/com/example/javier/ukelelearrospi/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.javier.ukelelearrospi;
 
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -28,7 +37,99 @@ public class MainActivity extends AppCompatActivity {
             toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
         }
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        drawerraKudeatu();
+
 	}
+
+    //TODO Hemen doiaz tokatzen dien fragmentak drawerretik. Berri bat gehitzeko menu/drawer.xml gehitu eta jarritako id-akin fragmentua aukeratu
+    private void onNavigationMenuItemSelected(MenuItem menuItem){
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.drawer_home:
+                //TODO tokatzen dan fragmentua jarri kasu bakoitzean
+                fragment = null; // fragment = new homeFragment()
+                break;
+            case R.id.drawer_main_chords:
+                openChords(null);
+                break;
+            case R.id.drawer_main_wikipedia:
+                openWikipedia(null);
+                break;
+            case R.id.drawer_main_learn:
+                openLearn(null);
+                break;
+        }
+
+        //TODO Fragmentu bezela jartzean deskomentatu
+        /*
+
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        menuItem.setChecked(true);
+        mDrawerLayout.closeDrawers();
+        */
+    }
+
+    private void drawerraKudeatu(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                onNavigationMenuItemSelected(menuItem);
+                return true;
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
 
     // http://blog.raffaeu.com/archive/2015/04/11/android-and-the-transparent-status-bar.aspx
     public int getStatusBarHeight() {
@@ -38,6 +139,31 @@ public class MainActivity extends AppCompatActivity {
             result = getResources().getDimensionPixelSize(resourceId);
         }
         return result;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 
 	@Override
