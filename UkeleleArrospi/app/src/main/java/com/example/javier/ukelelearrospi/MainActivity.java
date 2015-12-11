0a0b4e7d521bc509d46e503d87c98b1bcec8b5b1
aorbegozo005 <aorbegozo005@ikasle.ehu.es>
2015-12-11 23:46:28 +0100

    Toolbarra jarri

--- UkeleleArrospi/app/src/main/java/com/example/javier/ukelelearrospi/MainActivity.java
+++ UkeleleArrospi/app/src/main/java/com/example/javier/ukelelearrospi/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        //Toolbarra kargatu
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 	}
 
 	@Override
